ab7a5c01a858acce2080eed0374a8fca01090be5
RadLeo OFC <radleo.official@gmail.com>
2026-04-05 22:50:49 +0300

    Some updates in game, bun unsuccessfull

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-var zombie = document.getElementById("zombie");
-var cowboy = document.getElementById("cowboy");
-var bullet = document.getElementById("bullet");
+const zombie = document.getElementById("zombie");
+const cowboy = document.getElementById("cowboy");
+const bullet = document.getElementById("bullet");
+
+const zombiePosition = zombie.getBoundingClientRect();
+const cowboyPosition = cowboy.getBoundingClientRect();
+const bulletPosition = bullet.getBoundingClientRect();
+
+const zombieLeft = zombiePosition.left;
+const bulletLeft = bulletPosition.left;
+const cowboyLeft = cowboyPosition.left;
 
 document.addEventListener("keydown", function (event) {
   shoot();
 });
+
+function zombieKilled(zombieLeft, bulletLeft) {
+  return zombieLeft === bulletLeft;
+}
+
+function cowboyKilled(zombieLeft, cowboyLeft) {
+  return zombieLeft === cowboyLeft;
+}
 
 function shoot() {
   if (bullet.classList != "shoot") {
     bullet.classList.add("shoot");
   }
   setTimeout(function () {
-    if (zombieLeft == bulletLeft) {
+    if (zombieKilled) {
+      alert("YOU WON");
       bullet.classList.remove("shoot");
+    } else if (cowboyKilled) {
+      alert("YOU LOST");
     }
   });
 }
